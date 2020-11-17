f526b2ca12cbb428aa3a31a49d08b17137a1778a
Vincent <viproz12@gmail.com>
2020-11-17 21:25:22 +0100

    Update form-util.js
    Fixed privacy issue

--- src/js/form-util.js
+++ src/js/form-util.js
@@ -198,7 +198,7 @@ export function prepareInputs (formInputs, reasonInputs, reasonFieldset, reasonA
     updateSecureLS(formInputs)
     const pdfBlob = await generatePdf(getProfile(formInputs), reasons, pdfBase)
 
-    const creationInstant = new Date()
+    const creationInstant = releaseDateInput.value
     const creationDate = creationInstant.toLocaleDateString('fr-CA')
     const creationHour = creationInstant
       .toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' })
